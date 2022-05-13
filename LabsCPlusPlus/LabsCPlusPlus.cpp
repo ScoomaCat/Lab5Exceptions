cbe3efd5783d402d745a568de50c34610f2327f4
jayjay.meow <jayjay.meow@gmail.com>
2022-05-14 02:07:34 +0300

    Implement with actually following tech description

--- LabsCPlusPlus/LabsCPlusPlus.cpp
+++ LabsCPlusPlus/LabsCPlusPlus.cpp
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <windows.h>
 #include <excpt.h>
+#include <eh.h>
+#include <exception>
 
 void InitFirstTask() {
 	std::cout << "[First task start]" << std::endl;
@@ -29,14 +31,26 @@ void InitFirstTask() {
 	std::cout << "[First task end]\n";
 }
 
+//Floats won't generate exception without calling EnableFloatExeptions(). Will just return inf or NaN
+//For more info about this topic read docs at https://docs.microsoft.com/en-us/cpp/c-runtime-library/reference/controlfp-s?view=msvc-170
+void EnableFloatExeptions(unsigned int newControlWord)
+{
+	_clearfp();
+	_controlfp_s(0, newControlWord, _MCW_EM);
+}
+
 void InitSecondTask() {
 	std::cout << "[Second task start]" << std::endl;
 
 	__try
 	{
-		int a = 1; //Doubles won't generate exception! Will just return inf or NaN
-		int b = 0;
-		double c = a / b;
+		unsigned int oldControlWord;
+		_controlfp_s(&oldControlWord, 0, 0);
+		EnableFloatExeptions(oldControlWord & ~_EM_ZERODIVIDE | _EM_OVERFLOW);
+
+		double a = 1;
+		double b = 0;
+		double c = a / b; 
 	}
 	__except (EXCEPTION_EXECUTE_HANDLER)
 	{
@@ -44,9 +58,9 @@ void InitSecondTask() {
 
 		std::cout << "Exception code: " << code << std::endl;
 
-		if (code == EXCEPTION_INT_DIVIDE_BY_ZERO)
+		if (code == EXCEPTION_FLT_DIVIDE_BY_ZERO)
 		{
-			std::cout << "Caught the expected EXCEPTION_INT_DIVIDE_BY_ZERO exception!" << std::endl;
+			std::cout << "Caught the expected EXCEPTION_FLT_DIVIDE_BY_ZERO exception!" << std::endl;
 		}
 		else {
 			std::cout << "Ooops. Something went wrong, that's a diff exception!" << std::endl;
@@ -56,32 +70,54 @@ void InitSecondTask() {
 	std::cout << "[Second task end]" << std::endl;
 }
 
+class SE_Exception : public std::exception
+{
+private:
+	const unsigned int nSE;
+public:
+	SE_Exception() noexcept : SE_Exception{ 0 } {}
+	SE_Exception(unsigned int n) noexcept : nSE{ n } {}
+	unsigned int getSeNumber() const noexcept { return nSE; }
+};
 
-void InitThirdTask() {
+class Scoped_SE_Translator
+{
+private:
+	const _se_translator_function old_SE_translator;
+public:
+	Scoped_SE_Translator(_se_translator_function new_SE_translator) noexcept
+		: old_SE_translator{ _set_se_translator(new_SE_translator) } {}
+	~Scoped_SE_Translator() noexcept { _set_se_translator(old_SE_translator); }
+};
+
+void trans_func(unsigned int u, EXCEPTION_POINTERS*)
+{
+	throw SE_Exception(u);
+}
+
+//[[[!!!!!WARNING!!!!!]]]] 
+//Check your project properties before executing this code!
+//Go to Project Properties -> Configuration Properties -> C/C++ and set Enable C++ Exceptions to /EHa
+//This code will NOT work otherwise! 
+//For more info about this topic read docs at https://docs.microsoft.com/en-us/cpp/c-runtime-library/reference/set-se-translator?view=msvc-170#example-catch-__try-exception-error
+void InitThirdTask()
+{
+	Scoped_SE_Translator scoped_se_translator{ trans_func };
 	std::cout << "[Third task start]" << std::endl;
 
 	try
 	{
-		int* p = 0x00000000;   // pointer to NULL
-		*p = 13;    // causes an access violation exception;
+		int* p = 0x00000000;
+		*p = 13;
 	}
-	catch (const std::runtime_error& re)
+	catch (const SE_Exception& e)
 	{
-		// speciffic handling for runtime_error
-		std::cout << "Runtime error: " << re.what() << std::endl;
-	}
-	catch (const std::exception& ex)
-	{
-		// speciffic handling for all exceptions extending std::exception, except
-		// std::runtime_error which is handled explicitly
-		std::cout << "Error occurred: " << ex.what() << std::endl;
+		printf("Caught the expected exception, error %8.8x.\n", e.getSeNumber());
 	}
 	catch (...)
 	{
-		// catch any other errors (that we have no information about)
-		std::cout << "Unknown failure occurred. Possible memory corruption" << std::endl;
+		std::cout << "Unknown failure occurred." << std::endl;
 	}
-
 
 	std::cout << "[Third task end]" << std::endl;
 }
@@ -90,18 +126,7 @@ int main()
 {
 	InitFirstTask();
 	InitSecondTask();
-
-	__try
-	{
-		InitThirdTask();
-	}
-	__except (EXCEPTION_EXECUTE_HANDLER)
-	{
-		int code = GetExceptionCode();
-
-		std::cout << "Exception code: " << code << std::endl;
-		std::cout << "This exception was not caught by try-catch block, the program still fails! That's why we have SEH :D" << std::endl;
-	}
+	InitThirdTask();
 
 	std::cout << "[All tasks failed successfully]" << std::endl;
 }
